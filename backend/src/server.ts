8c9f1202b56a27401141a5251d67fa9fbd56ff72
lakshyakumar90 <lakshyakumar5023@gmail.com>
2026-01-05 20:20:36 +0530

    url shortner project init

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+
+const app = express();
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running on port 3000");
+});
+
+
