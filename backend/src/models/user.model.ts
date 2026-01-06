c0774f8c1de36109157cb1ccd478d8fba4fec871
lakshyakumar90 <lakshyakumar5023@gmail.com>
2026-01-06 14:55:09 +0530

    user model complete

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,5 +1,13 @@
-import {pgTable, uuid} from "drizzle-orm/pg-core";
+import {pgTable, text, timestamp, uuid, varchar} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
     id: uuid().primaryKey().defaultRandom(),
+    firstname: varchar("firstname", {length: 55}).notNull(),
+    lastname: varchar("lastname", {length: 55}).notNull(),
+    email: varchar("email", {length: 255}).notNull().unique(),
+    password: text().notNull(),
+    salt: text().notNull(),
+
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().$onUpdate(()=> new Date()),
 })
